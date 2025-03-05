c61ab01ab2c33b2b8ca191a749fbc28b79ddbe2f
Samuel <147870470+schevve@users.noreply.github.com>
2025-03-05 14:01:16 -0300

    changed text color in styles.css

--- projects.js
+++ projects.js
@@ -16,10 +16,10 @@ project_div_base.classList = 'project-div'
 document.addEventListener('DOMContentLoaded', () => {
     fetch_projects().then(data => {
         var projects = data;
+        console.log(data);
         for(let i = 0; i < projects.length; i++)
         {
             let project_div = project_div_base.cloneNode();
-            
             project_div.innerHTML = projects[i].name;
             document.getElementById("projects-table").appendChild(project_div);
         }
