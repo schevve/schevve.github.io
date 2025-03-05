0f740530102161785192061d6effb32936b6e917
Samuel <147870470+schevve@users.noreply.github.com>
2025-03-05 17:26:25 -0300

    project divs now are a's with its link and I give up explaining it the rest

--- projects.js
+++ projects.js
@@ -9,12 +9,17 @@ async function fetch_projects() {
     }
 }
 
+let project_div_a_container_base = document.createElement('a');
+project_div_a_container_base.classList = 'project-div'
 let project_div_base = document.createElement('div');
-project_div_base.classList = 'project-div'
 let project_title_base = document.createElement('h3');
 project_title_base.classList = 'project-title'
+let newtab_img_base = document.createElement('img');
+newtab_img_base.src = 'static/NewTab.png';
+newtab_img_base.classList = 'newtab-icon'
 let project_desc_base = document.createElement('p');
 project_desc_base.classList = 'project-desc'
+
 
 /**/
 document.addEventListener('DOMContentLoaded', () => {
@@ -23,21 +28,40 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(data);
         for(let i = 0; i < projects.length; i++)
         {
-            //instantiate a clone of project_div_base
+            //instantiate a clone of project_div_base and project_div_a_container
+            let project_div_a_container = project_div_a_container_base.cloneNode();
             let project_div = project_div_base.cloneNode();
+
+            //2 divs in the head of the project block
+            let head_div_1 = project_div_base.cloneNode();
+            head_div_1.classList = 'project-div-half'
+            project_div.appendChild(head_div_1);
+            let head_div_2 = project_div_base.cloneNode();
+            head_div_2.classList = 'project-div-half'
+            project_div.appendChild(head_div_2);
+
 
             //title
             let project_title = project_title_base.cloneNode();
             project_title.innerHTML = projects[i].name;
-            project_div.appendChild(project_title);
+            head_div_1.appendChild(project_title);
+
+            //icon new tab
+            let newtab_img = newtab_img_base.cloneNode();
+            head_div_2.appendChild(newtab_img);
 
             //description
             let project_desc = project_desc_base.cloneNode();
             project_desc.innerHTML = projects[i].description;
             project_div.appendChild(project_desc);
+
+            //add link to a
+            project_div_a_container.href = projects[i].html_url;
+            project_div_a_container.target = '_blank'
             
-            //append div to table
-            document.getElementById("projects-table").appendChild(project_div);
+            //append div to a and a to table
+            project_div_a_container.appendChild(project_div);
+            document.getElementById("projects-table").appendChild(project_div_a_container);
         }
         
     });
