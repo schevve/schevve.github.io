8eb67ff286f8645d235864388e9ad04d0fc171e5
schevve <samuel.cardoso@alunos.bpkedu.com.br>
2025-05-19 21:27:31 -0300

    Modified index.html    removed wip from subtitle Modified styles.css    made the project blocks take the whole available width Modified projects.js    added creation date to project description

--- projects.js
+++ projects.js
@@ -11,15 +11,15 @@ async function fetch_projects() {
 
 // I don't want anyone to see this mess
 let project_div_a_container_base = document.createElement('a');
-project_div_a_container_base.classList = 'project-div'
+project_div_a_container_base.classList = 'project-box';
 let project_div_base = document.createElement('div');
 let project_title_base = document.createElement('h3');
 project_title_base.classList = 'project-title'
 let newtab_img_base = document.createElement('img');
 newtab_img_base.src = 'static/NewTab.png';
-newtab_img_base.classList = 'newtab-icon'
+newtab_img_base.classList = 'newtab-icon';
 let project_desc_base = document.createElement('p');
-project_desc_base.classList = 'project-desc'
+project_desc_base.classList = 'project-desc';
 
 document.addEventListener('DOMContentLoaded', () => {
     fetch_projects().then(data => {
@@ -33,10 +33,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
             //2 divs in the head of the project block
             let head_div_1 = project_div_base.cloneNode();
-            head_div_1.classList = 'project-div-half'
+            head_div_1.classList = 'project-div-half left';
             project_div.appendChild(head_div_1);
             let head_div_2 = project_div_base.cloneNode();
-            head_div_2.classList = 'project-div-half'
+            head_div_2.classList = 'project-div-half right';
             project_div.appendChild(head_div_2);
 
 
@@ -52,11 +52,17 @@ document.addEventListener('DOMContentLoaded', () => {
             //description
             let project_desc = project_desc_base.cloneNode();
             project_desc.innerHTML = projects[i].description;
+            
+            project_div.appendChild(project_desc);
+
+            project_desc = project_desc_base.cloneNode();
+            let creation_date = new Date(projects[i].created_at);
+            project_desc.innerHTML += creation_date.toLocaleDateString();
             project_div.appendChild(project_desc);
 
             //add link to a
             project_div_a_container.href = projects[i].html_url;
-            project_div_a_container.target = '_blank'
+            project_div_a_container.target = '_blank';
             
             //append div to a and a to table
             project_div_a_container.appendChild(project_div);
